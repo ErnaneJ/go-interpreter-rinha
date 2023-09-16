341691fab587dc406ab6c641e35bdc317e6e342b
Ernane Ferreira <ernane.junior25@gmail.com>
2023-09-16 17:15:46 -0300

    fix: updates *.rinha.* file path according to specifications

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		panic("To execute the desired program prediction. for example: 'go run print' to interpret the file './var/rinha/files/print.rinha.json'")
+		panic("To execute the desired program prediction. for example: 'go run print' to interpret the file './var/rinha/print.rinha.json'")
 	}
 
 	fileName := os.Args[1]
 
-	astExpression, err := executor.Execute("./var/rinha/files/" + fileName + ".rinha.json")
+	astExpression, err := executor.Execute("./var/rinha/" + fileName + ".rinha.json")
 
 	if err != nil {
 		panic(err)
